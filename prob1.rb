9a0fe801a9c5daf1bd2d790266b8e7bfcdc8f6d2
CawWoo <melodicraven@gmail.com>
2024-03-25 21:06:51 -0500

    Solved problem 1

--- prob1.rb
+++ prob1.rb
@@ -1,1 +1,41 @@
 #Fetch and analyze public repository data from the GitHub API for a specific user.
+
+require 'httparty'
+require 'json'
+
+username = 'moxie0'
+
+# Send GET request
+response = HTTParty.get("https://api.github.com/users/#{username}/repos")
+# Parse the JSON response
+repos = JSON.parse(response.body)
+
+#Variable tracking
+max_stars = 0
+most_starred_repo = nil
+
+#Go through the repositories
+repos.each do |repo|
+  name = repo['name']
+  description = repo['description']
+  stars = repo['stargazers_count']
+  url = repo['html_url']
+
+  # Check for repositories that have more stars than max
+  if stars > max_stars
+    max_stars = stars
+    most_starred_repo = {
+      name: name,
+      description: description,
+      stars: stars,
+      url: url
+    }
+  end
+end
+
+#Information
+puts "Most Starred Repository:"
+puts "Name: #{most_starred_repo[:name]}"
+puts "Description: #{most_starred_repo[:description]}"
+puts "Stars: #{most_starred_repo[:stars]}"
+puts "URL: #{most_starred_repo[:url]}"
